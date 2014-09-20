7d033edaea7b3e1e7e43d59c65c8a905b2c0b01a
nikit.cpp <nikit.cpp@yandex.ru>
2014-09-20 21:26:09 +0300

    original from https://github.com/kreed/vanilla

--- src/com/github/nikit/cpp/core/NonEditableInfo.java
+++
@@ -1,5 +0,0 @@
-package com.github.nikit_cpp;
-
-public enum NonEditableInfo {
-	FILE_SIZE, DURATION, BITRATE, CREATION_DATE, LAST_ACCESS_DATE, MODIFICATION_DATE;
-}
--- src/com/github/nikit/cpp/core/Time.java
+++
@@ -1,5 +0,0 @@
-package com.github.nikit_cpp;
-
-public interface Time {
-
-}
