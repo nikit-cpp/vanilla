870f99d511d1d72789e11d88765c3abbe39a06f5
nikit.cpp <nikit.cpp@yandex.ru>
2014-09-27 01:13:01 +0300

    работает. захачен listview

--- src/main/java/com/github/nikit/cpp/activity/LibraryActivity.java
+++ src/main/java/com/github/nikit/cpp/activity/LibraryActivity.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.TextView;
+import com.github.nikit.cpp.aa.PlayListAdapter;
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 import org.kreed.vanilla.R;
@@ -17,12 +19,11 @@ public class LibraryActivity extends Activity {
     @ViewById
     ListView listContent;
 
-    /*@Override public void onCreate(Bundle state) {
-        super.onCreate(state);
-        setContentView( R.layout.full_playback);
-    }*/
-    @AfterViews
-    void initViews() {
+    @Bean
+    PlayListAdapter adapter;
 
+    @AfterViews
+    void bindAdapter() {
+        listContent.setAdapter(adapter);
     }
 }
