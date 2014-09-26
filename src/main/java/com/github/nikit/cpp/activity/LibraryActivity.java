bf7c622d3aa0ff11c3e8243bdbb701256b30cdde
nikit.cpp <nikit.cpp@yandex.ru>
2014-09-27 00:00:14 +0300

    починил

--- src/main/java/com/github/nikit/cpp/activity/LibraryActivity.java
+++ src/main/java/com/github/nikit/cpp/activity/LibraryActivity.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.TextView;
+import com.github.nikit.cpp.aa.PlayListAdapter;
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 import org.kreed.vanilla.R;
@@ -21,6 +23,15 @@ public class LibraryActivity extends Activity {
         super.onCreate(state);
         setContentView( R.layout.full_playback);
     }*/
+
+    @Bean
+    PlayListAdapter adapter;
+
+    @AfterViews
+    void bindAdapter() {
+        listContent.setAdapter(adapter);
+    }
+
     @AfterViews
     void initViews() {
 
