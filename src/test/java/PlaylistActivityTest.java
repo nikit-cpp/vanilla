7cd6f71ce7ff76b050616699041d71491a820617
nik <nikit.cpp@yandex.ru>
2014-10-05 01:32:54 +0400

    PlaylistActivityTest теперь работает

--- src/test/java/PlaylistActivityTest.java
+++ src/test/java/PlaylistActivityTest.java
@@ -5,6 +5,7 @@ import com.github.nikit.cpp.activity.FullPlaybackActivity;
 import com.github.nikit.cpp.activity.FullPlaybackActivity_;
 import com.github.nikit.cpp.activity.LibraryActivity;
 import com.github.nikit.cpp.activity.LibraryActivity_;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -14,31 +15,52 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.shadows.ShadowListView;
 import org.robolectric.shadows.ShadowTextView;
+
 import static org.fest.assertions.api.Assertions.assertThat;
 
-import static org.hamcrest.CoreMatchers.equalTo;
 
 @RunWith(RobolectricTestRunner.class)
 public class PlaylistActivityTest {
 
     private LibraryActivity activity;
+    private ListView listContent;
 
     @Before
-    public void setup()  {
+    public void setup() {
         activity = Robolectric.buildActivity(LibraryActivity_.class).create().get();
+        listContent = (ListView) activity.findViewById(R.id.listContent);
     }
 
     @Ignore
     @Test
-    public void shouldHaveHappySmiles() throws Exception {
+    public void testGettingItemByText() throws Exception {
         ListView listView = new ListView(Robolectric.application);
-        ListView listContent = (ListView) activity.findViewById(R.id.listContent);
+
         ShadowListView shadowedList = Robolectric.shadowOf(listContent);
         //assertThat(shadowedList.findItemContainingText("Non-existent item").);
 
         System.out.println(shadowedList);
+
+        /**
+         * ошибка предположительно от того, что findItemContainingText() в своейреализации вызывает getChildAt(), которая не находит child view, т. к. я использую стандартные элементы списка
+         */
         View item1 = shadowedList.findItemContainingText("Big gun");
         assertThat(item1).isNotNull();
         assertThat(item1).isSameAs(listView.getChildAt(0));
+    }
+
+
+    @Test
+    public void testItems() {
+        ShadowListView shadowedList = Robolectric.shadowOf(listContent);
+        assertThat(shadowedList.findIndexOfItemContainingText("AC/DC")).isSameAs(0);
+        assertThat(shadowedList.findIndexOfItemContainingText("Black Sabbath")).isSameAs(1);
+        assertThat(shadowedList.findIndexOfItemContainingText("Ken Hensley")).isSameAs(2);
+    }
+
+    @Test
+    public void testSongsCounts() throws Exception {
+        // Утверждаем что у нас 3 песни
+        assertThat(listContent.getCount()).isSameAs(3);
     }
 } 
